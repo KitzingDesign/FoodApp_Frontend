7d141c3c180cbd6680383c89d6bf54effebb0343
Jakob Kitzing <Jakob.kitzing@hotmail.com>
2024-11-11 14:02:02 +0100

    connection to db

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3500",
+  baseUrl: "https://4jcheeatmh.eu-central-1.awsapprunner.com/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
