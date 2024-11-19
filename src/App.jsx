cc0f18d9ab16e162d4070e794260063b87c8a306
Jakob Kitzing <Jakob.kitzing@hotmail.com>
2024-11-19 14:53:21 +0100

    test fix vh on phone

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,19 @@ import PageNotFound from "./components/404/PageNotFound.jsx";
  *
  * Routes are organized into public, protected, and nested routes inside the dashboard.
  */
+function setVhUnit() {
+  // Get the viewport height and multiply it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+// Execute the function initially
+setVhUnit();
+
+// Execute the function on resize
+window.addEventListener("resize", setVhUnit);
+
 function App() {
   return (
     <Routes>
